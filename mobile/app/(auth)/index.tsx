b3348d1a428128082304a6124a0ba30b94ab6933
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2025-12-30 00:03:59 +0530

    feat(ui): add lottie animation for onboarding screen

--- mobile/app/(auth)/index.tsx
+++ mobile/app/(auth)/index.tsx
@@ -1,75 +1,81 @@
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { OnboardingStyle } from "@/assets/styles/onboarding-style";
-import { router } from "expo-router";
-import Button from "@/components/button";
+import Onboarding from "react-native-onboarding-swiper";
+import LottieView from "lottie-react-native";
+import { Colors } from "@/lib/constants";
 
-const Onboarding = () => {
+const OnboardingScreen = () => {
   return (
-    <ScrollView
-      contentContainerStyle={OnboardingStyle.container}
-      showsVerticalScrollIndicator={false}
-    >
-      <SafeAreaView style={OnboardingStyle.subContainer}>
-        <View style={OnboardingStyle.header}>
-          <View style={OnboardingStyle.logoContainer}>
-            <Text style={OnboardingStyle.logoMain}>Fit</Text>
-            <Text style={OnboardingStyle.logoAccent}>Freak</Text>
-          </View>
-          <View style={OnboardingStyle.badge}>
-            <Text style={OnboardingStyle.badgeText}>
-              Your Fitness Gym Companion
-            </Text>
-          </View>
-        </View>
+    <View style={styles.container}>
+      <Onboarding
+        pages={[
+          {
+            backgroundColor: Colors.secondary,
+            image: (
+              <View>
+                <LottieView
+                  source={require("../../assets/animations/Weightlifting competition")}
+                  autoPlay
+                  loop
+                  style={styles.lottie}
+                />
+              </View>
+            ),
+            title: "Onboarding Screen",
+            subtitle:
+              "Done with React native Onboarding Swiper this is better Pretty",
+          },
 
-        <View style={OnboardingStyle.imageContainer}>
-          <Image
-            source={require("../../assets/images/fitness.png")}
-            style={OnboardingStyle.image}
-          />
-        </View>
+          {
+            backgroundColor: "#FEFBF3",
+            image: (
+              <View>
+                <LottieView
+                  source={require("../../assets/animations/Gym dubble.json")}
+                  autoPlay
+                  loop
+                  style={styles.lottie}
+                />
+              </View>
+            ),
+            title: "Main Screen",
+            subtitle:
+              "Done with React native Onboarding Swiper this is better Pretty",
+          },
 
-        <View style={OnboardingStyle.content}>
-          <Text style={OnboardingStyle.heading}>
-            Track your <Text style={OnboardingStyle.headingAccent}>GYM</Text>{" "}
-            journey
-          </Text>
-
-          <View style={OnboardingStyle.features}>
-            <View style={OnboardingStyle.featureItem}>
-              <View style={OnboardingStyle.featureDot} />
-              <Text style={OnboardingStyle.featureText}>Monitor workouts</Text>
-            </View>
-            <View style={OnboardingStyle.featureItem}>
-              <View style={OnboardingStyle.featureDot} />
-              <Text style={OnboardingStyle.featureText}>Track progress</Text>
-            </View>
-            <View style={OnboardingStyle.featureItem}>
-              <View style={OnboardingStyle.featureDot} />
-              <Text style={OnboardingStyle.featureText}>Reach your goals</Text>
-            </View>
-          </View>
-        </View>
-
-        <View style={OnboardingStyle.actions}>
-          <Button
-            onPress={() => router.push("/sign-in")}
-            text="Sign In"
-            variant="dark"
-            customStyle={OnboardingStyle.primaryButton}
-          />
-          <Button
-            onPress={() => router.push("/sign-up")}
-            text="Sign Up"
-            variant="light"
-            customStyle={OnboardingStyle.secondaryButton}
-          />
-        </View>
-      </SafeAreaView>
-    </ScrollView>
+          {
+            backgroundColor: "#F0FDF9",
+            image: (
+              <View>
+                <LottieView
+                  source={require("../../assets/animations/Weightlifting competition")}
+                  autoPlay
+                  loop
+                  style={styles.lottie}
+                />
+              </View>
+            ),
+            title: "Super Screen",
+            subtitle:
+              "Done with React native Onboarding Swiper this is better Pretty",
+          },
+        ]}
+        // bottomBarColor="black"
+      />
+    </View>
   );
 };
 
-export default Onboarding;
+export default OnboardingScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.white,
+  },
+
+  lottie: {
+    width: 300,
+    height: 300,
+  },
+});
